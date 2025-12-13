a7a09a2c6d87a54600cc7ad29177a45fd375919d
Fernando Castro <fernand4603b@gmail.com>
2025-12-13 20:02:43 +0000

    tentando fazer a do estacionamento

--- tasks/poo/estacionamento/py/draft.py
+++ tasks/poo/estacionamento/py/draft.py
@@ -1,5 +1,29 @@
-class Veiculo:
-    def __init__(self, id:str, tipo:str, hE:int):
+from abc import ABC, abstractmethod
+
+class Veiculo(ABC):
+    def __init__(self, id:str, entrada, tipo:str):
+        super().__init__()
+        self.id = id
+        self.entrada:None=entrada
+        self.tipo=tipo
+
+    def getEntrada(self):
+        return self.entrada
+    
+    def setEntrada(self, entrada):
+        self.entrada = entrada
+    
+    @abstractmethod
+    def calcularValor(self,horaSaida):
+        pass
+
+    def __str__(self):
+        print(f"{self.tipo} : {self.id} : {self.entrada}")
+
+    
+
+
+    
         
         
         
